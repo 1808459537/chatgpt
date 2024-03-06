46d74c31efdd579eca873133f911d10d62958242
ZHT <71699103+1808459537@users.noreply.github.com>
2024-03-06 17:30:20 +0800

    文档

--- src/test/java/com/zht/chatgptapi/ChatgptApiApplicationTests.java
+++ src/test/java/com/zht/chatgptapi/ChatgptApiApplicationTests.java
@@ -1,11 +1,18 @@
 package com.zht.chatgptapi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
+@SpringBootApplication
+@RestController
 class ChatgptApiApplicationTests {
-
     @Test
+
     void contextLoads() {
         System.out.println("111");
     }
