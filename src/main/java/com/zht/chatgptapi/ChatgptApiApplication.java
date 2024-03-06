a5d76e2fe60446367216950f250af8a175fef87e
ZHT <71699103+1808459537@users.noreply.github.com>
2024-03-06 12:44:14 +0800

    文档

---
+++ src/main/java/com/zht/chatgptapi/ChatgptApiApplication.java
@@ -0,0 +1,13 @@
+package com.zht.chatgptapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zht/chatgptapi/ChatgptApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zht.chatgptapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class ChatgptApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("111");
+    }
+
+}
